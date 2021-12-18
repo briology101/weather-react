03899194ab73ce7bf4d7a984a0fc9264c41cfffc
briology101 <85913910+briology101@users.noreply.github.com>
2021-12-17 20:34:24 -0500

    Test update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
     <header className="App-header">
-      <h1>Hello dude</h1>
+      <h1>Hello friend</h1>
       <Weather city="Chicago" />
       </header>
     </div>

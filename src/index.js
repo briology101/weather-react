029ef57b591423cb822199cd33c9380dfcb0c142
briology101 <85913910+briology101@users.noreply.github.com>
2021-12-17 21:18:55 -0500

    bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './styles.css';
+import "bootstrap/dist/css/bootstrap.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
